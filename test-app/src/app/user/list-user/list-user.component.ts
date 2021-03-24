11493da5560dd41afd3c13809370208ce8a1ad2c
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-24 12:50:31 +0530

    Directives

--- test-app/src/app/user/list-user/list-user.component.ts
+++ test-app/src/app/user/list-user/list-user.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUserComponent implements OnInit {
 
+  users: UserModel[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    let user = new UserModel();
+    user.id = 1;
+    user.username = "User1";
+    this.users.push(user);
+    let user2 = new UserModel();
+    user2.id = 2;
+    user2.username = "User2";
+    this.users.push(user2);
+    let user3 = new UserModel();
+    user3.id = 3;
+    user3.username = "User3";
+    this.users.push(user3);
   }
 
+}
+
+class UserModel {
+  username: string;
+  id: number;
 }
--- test-app/src/app/user/user-form/user-form.component.ts
+++ test-app/src/app/user/user-form/user-form.component.ts
@@ -15,3 +15,4 @@ export class UserFormComponent implements OnInit {
   }
 
 }
+
