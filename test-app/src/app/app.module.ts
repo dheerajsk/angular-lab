9a34c94427409ed4671ea81fabe748faa6c619f6
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-24 11:30:28 +0530

    Initial App

---
+++ test-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { AnotherComponent } from './another/another.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnotherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
