9865d1a8a1ee928db36f9c03246489bf457cdc65
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-26 12:10:31 +0530

    Pipes

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -9,8 +9,11 @@ export class AppComponent {
   title = 'test-app';
   data = "This is from Parent variable";
   message = "Waiting for user to submit form";
-  pclass="pmesssage";
-  pcolor="white";
+  pclass = "pmesssage";
+  pcolor = "white";
+  subtitle = "Welcome to my page. Please use menus to navigate";
+
+  dateVal: Date = new Date();
 
   handleUserFormClickEvent(event) {
     this.message = "Form is submitted by " + event;
